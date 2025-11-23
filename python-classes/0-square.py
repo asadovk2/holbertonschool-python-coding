429dd4b99c392d834a90a5078fac344c1ff2f2c7
asadovk2 <kamalaa@code.edu.az>
2025-11-23 06:17:52 +0000

    f

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-
-
-"""Example Google style docstrings."""
+"""
+This module defines the Square class.
+"""
 
 
 class Square:
-    """The summary line for a class docstring should fit on one line."""
-    pass
+    """
+    Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a square with a private size attribute.
+        No type or value validation is performed.
+        """
+        self.__size = size
